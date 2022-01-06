af56e132f099a67f80e66a92d97013f41778497a
quanvo0213 <quan.vo@katalon.com>
2022-01-06 10:42:10 +0700

    first commit

---
+++ Test Suites/Test Suite Quân á à ạ ã ả.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test Suite Quân á à ạ ã ả</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>e18d693a-f35a-4627-9017-a18406d0ecdd</testSuiteGuid>
+</TestSuiteEntity>
