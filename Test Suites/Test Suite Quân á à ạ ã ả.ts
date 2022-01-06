7e87a5b776034daefaf8b794970e2f24f41d8901
quanvo0213 <quan.vo@katalon.com>
2022-01-06 11:02:54 +0700

    add tc

--- Test Suites/Test Suite Quân á à ạ ã ả.ts
+++ Test Suites/Test Suite Quân á à ạ ã ả.ts
@@ -5,10 +5,22 @@
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
-   <numberOfRerun>0</numberOfRerun>
+   <numberOfRerun>3</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
-   <rerunImmediately>false</rerunImmediately>
+   <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>e18d693a-f35a-4627-9017-a18406d0ecdd</testSuiteGuid>
+   <testCaseLink>
+      <guid>11037e23-af30-48fa-97c3-11e9061f0324</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Folder 2/TestCase Quân á à ạ ã ả</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>0df7c54b-551a-436f-b691-b9f2aed49a86</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Folder 1/TestCase Quân á à ạ ã ả</testCaseId>
+   </testCaseLink>
 </TestSuiteEntity>
