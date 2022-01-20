73e8c799e01473f30e2db7d60a364be100f262e8
quanvo0213 <quan.vo@katalon.com>
2022-01-20 14:33:46 +0700

    rename

--- Test Suites/TSC 1.ts
+++ Test Suites/TSC 1.ts
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <TestSuiteCollectionEntity>
    <description></description>
-   <name>Test Suite Collection Quân á à ạ ã ả</name>
+   <name>TSC 1</name>
    <tag></tag>
    <executionMode>SEQUENTIAL</executionMode>
    <maxConcurrentInstances>8</maxConcurrentInstances>
--- Test Suites/Test Suite 1.ts
+++ Test Suites/Test Suite 1.ts
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <TestSuiteEntity>
    <description></description>
-   <name>Test Suite Quân á à ạ ã ả</name>
+   <name>Test Suite 1</name>
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
@@ -15,24 +15,24 @@
       <guid>11037e23-af30-48fa-97c3-11e9061f0324</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Folder 2/TestCase Quân á à ạ ã ả</testCaseId>
+      <testCaseId>Test Cases/Folder 2/TestCase 3</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>0df7c54b-551a-436f-b691-b9f2aed49a86</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Folder 1/TestCase Quân á à ạ ã ả</testCaseId>
+      <testCaseId>Test Cases/Folder 1/TestCase 1</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>74caf8d3-c31e-4485-8c07-aa5a316c1715</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Folder 2/Tiếng Tàu nè 我爱你</testCaseId>
+      <testCaseId>Test Cases/Folder 2/TestCase 4</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>a54d76a2-199f-413e-b812-7c81b728c6dc</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Folder 2/Tiếng Tàu nè 我爱你</testCaseId>
+      <testCaseId>Test Cases/Folder 2/TestCase 4</testCaseId>
    </testCaseLink>
 </TestSuiteEntity>
